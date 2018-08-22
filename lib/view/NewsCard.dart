dfd37dd1a102841a79de4141697620144a7f0b1a
Pablo Reinoso <pabloreinoso0@gmail.com>
2018-08-21 21:09:32 -0400

    007 - Creando estructura de la Card

--- lib/view/NewsCard.dart
+++ lib/view/NewsCard.dart
@@ -10,7 +10,35 @@ class _NewsCard extends State<NewsCard>{
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: Text('TARJETA'),
+      child: Column(
+        children: <Widget>[
+          Stack(
+            alignment: Alignment(0.0, 1.0),
+            children: <Widget>[
+              Image.network('http://a57.foxnews.com/media2.foxnews.com/BrightCove/694940094001/2018/08/21/0/0/694940094001_5825037545001_5825036631001-vs.jpg'),
+              Container(
+                child: Column(
+                  children: <Widget>[
+                    Text('Titulo',
+                    style: new TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: Theme.of(context).textTheme.title.fontSize
+                    ),
+                    ),
+                    Text('Descripcion de la noticia',
+                      style: new TextStyle(
+                          color: Colors.white
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+          Text('TARJETA'),
+        ],
+      ),
     );
   }
 
