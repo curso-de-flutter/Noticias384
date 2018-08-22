2172d4abd442e5c3f661d8880f1df51354e7e515
Pablo Reinoso <pabloreinoso0@gmail.com>
2018-08-21 20:46:00 -0400

    Creando Home y Personalizando nuestra app

---
+++ lib/view/Home.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget{
+
+  @override
+  State createState() => _homeState();
+}
+class _homeState extends State<Home>{
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: new AppBar(
+        title: Text('Noticias384'),
+      ),
+      body: Text('HOME'),
+    );
+  }
+}
