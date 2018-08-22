b7a32f45dd2bd39c11109791f68a997436bffd54
Pablo Reinoso <pabloreinoso0@gmail.com>
2018-08-22 19:06:50 -0400

    009 - Terminando de personalizar Card

--- lib/view/NewsCard.dart
+++ lib/view/NewsCard.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 class NewsCard extends StatefulWidget{
-
   @override
   State createState() => _NewsCard();
 }
@@ -10,41 +9,53 @@ class _NewsCard extends State<NewsCard>{
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: Column(
-        children: <Widget>[
-          new Stack(
-            alignment: Alignment(0.0, 1.0),
-            children: <Widget>[
-              Image.network('http://a57.foxnews.com/media2.foxnews.com/BrightCove/694940094001/2018/08/21/0/0/694940094001_5825037545001_5825036631001-vs.jpg'),
-              Container(
-                width: MediaQuery.of(context).size.width,
-                decoration: new BoxDecoration(
-                  color: Color.fromRGBO(0, 0, 0, 0.7),
-                  shape: BoxShape.rectangle,
-                  borderRadius: BorderRadius.circular(5.0)
-                ),
-                child: new Column(
-                  children: <Widget>[
-                    new Text('Titulo',
-                    style: new TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontSize: Theme.of(context).textTheme.title.fontSize
-                    ),
-                    ),
-                    new Text('Descripcion de la noticia',
+      child: Container(
+        height: 290.0,
+        child: new Column(
+          children: <Widget>[
+            new Stack(
+              alignment: Alignment(0.0, 1.0),
+              children: <Widget>[
+                Image.network('http://a57.foxnews.com/media2.foxnews.com/BrightCove/694940094001/2018/08/21/0/0/694940094001_5825037545001_5825036631001-vs.jpg'),
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  decoration: new BoxDecoration(
+                    color: Color.fromRGBO(0, 0, 0, 0.7),
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.circular(5.0)
+                  ),
+                  child: new Column(
+                    children: <Widget>[
+                      new Text('Titulo',
                       style: new TextStyle(
-                          color: Colors.white
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: Theme.of(context).textTheme.title.fontSize
                       ),
-                    )
-                  ],
-                ),
-              )
-            ],
-          ),
-          Divider(color: Colors.black,),
-          Text('TARJETA'),
-        ],
+                      ),
+                      new Text('Descripcion de la noticia',
+                        style: new TextStyle(
+                            color: Colors.white
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+            Divider(color: Colors.black,),
+            Container(
+              child: Row(
+                children: <Widget>[
+                  FlatButton(
+                    onPressed: (){},
+                    child: Text('Ver Noticia'),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
