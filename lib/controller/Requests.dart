e0a29339ac113b1920415dd2a2d975e53d3d7e2b
Pablo Reinoso <pabloreinoso0@gmail.com>
2018-08-24 21:15:09 -0400

    016 - Analizando datos que utilizaremos

--- lib/controller/Requests.dart
+++ lib/controller/Requests.dart
@@ -5,7 +5,7 @@ import 'package:http/http.dart' as http;
 class Requests{
 
   static Future<dynamic> petiionNoticias() async{
-    var respuesta = await http.get('https://newsapi.org/v2/top-headlines?country=us&apiKey=ff0f32791a5c466ebbfc382378fc5746');
+    var respuesta = await http.get('https://newsapi.org/v2/top-headlines?language=es&apiKey=ff0f32791a5c466ebbfc382378fc5746');
 
     if (respuesta.statusCode == 200){
       var jsonRespuesta = json.decode(respuesta.body);
